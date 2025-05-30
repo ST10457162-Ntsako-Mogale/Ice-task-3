803dcf736adeb217b9578db19f3dac179a566b13
ST10457162-Ntsako-Mogale <ST10457162@vcconnect.edu.za>
2025-05-30 18:47:12 +0200

    2nd commit

---
+++ ice task 3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+namespace ice_task_3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+
+    
+}
